207df773b669464b04a36ffe23726c5d7564534a
Raif Olson <raif.t.olson.24@dartmouth.edu>
2024-03-25 23:31:27 -0400

    fix bug from PR

--- supervision/tracker/byte_tracker/core.py
+++ supervision/tracker/byte_tracker/core.py
@@ -289,10 +289,16 @@ class ByteTrack:
             for i, idet, itrack in enumerate(matches):
                 if i == 0:
                     final_detections = detections[[idet]]
-                    final_detections.tracker_id[0] = int(tracks[itrack].track_id)
+                    if final_detections.tracker_id is None:
+                        final_detections.tracker_id = [int(tracks[itrack].track_id)]
+                    else:
+                        final_detections.tracker_id[0] = int(tracks[itrack].track_id)
                 else:
-                    current_detection = detections[[idet]]
-                    current_detection.tracker_id[0] = int(tracks[itrack].track_id)
+                    current_detection = detections[[idet]]                    
+                    if current_detection.tracker_id is None:
+                        current_detection.tracker_id = [int(tracks[itrack].track_id)]
+                    else:
+                        current_detection.tracker_id[0] = int(tracks[itrack].track_id)
                     final_detections = Detections.merge(
                         [final_detections, current_detection]
                     )
